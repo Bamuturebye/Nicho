2207d95e37a54267c01ab85ebafe2f8306fcdcfb
DigBit <nickie8283@gmail.com>
2020-02-13 19:16:48 +0300

    hellooooo

--- app/src/main/java/com/example/nicho/MainActivity.java
+++ app/src/main/java/com/example/nicho/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.nicho;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void sendMessage(View view){
+
+        EditText message;
+        message = (EditText)findViewById(R.id.textView);
+        Toast.makeText(this, "Sending message" + message.getText().toString(),
+                Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        intent.putExtra("MESSAGE", message.getText().toString());
+        startActivity(intent);
+
+        message.setText("");
+
     }
 }
